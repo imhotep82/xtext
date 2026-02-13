4345242c79e175f50340eacd1c20d74a8862c2de
Hannes Wellmann <wellmann.hannes1@gmx.net>
2026-02-13 18:32:20 +0100

    [Build] Update to Tycho 5.0.2
    Part of
    - https://github.com/eclipse-xtext/xtext/issues/3509
    
    Co-authored-by: Christian Dietrich <christian.dietrich.opensource@gmail.com>

--- org.eclipse.xtext.xtext.wizard/xtend-gen/org/eclipse/xtext/xtext/wizard/ParentProjectDescriptor.java
+++ org.eclipse.xtext.xtext.wizard/xtend-gen/org/eclipse/xtext/xtext/wizard/ParentProjectDescriptor.java
@@ -105,7 +105,7 @@ public class ParentProjectDescriptor extends ProjectDescriptor {
   }
 
   public String getTychoVersion() {
-    return "4.0.13";
+    return "5.0.2";
   }
 
   private CharSequence loadResource(final String resourcePath) {
