7a2d3cafbe62b7679bd350d169405092c4bcbe50
Christian Dietrich <christian.dietrich.opensource@gmail.com>
2026-08-16 15:29:35 +0200

    MWE 2.27.0 M3 (#3769)

--- org.eclipse.xtext.util/src/org/eclipse/xtext/util/XtextVersion.java
+++ org.eclipse.xtext.util/src/org/eclipse/xtext/util/XtextVersion.java
@@ -52,14 +52,14 @@ public class XtextVersion {
 	 * @noreference
 	 */
 	public String getMweVersion() {
-		return "2.27.0.M1";
+		return "2.27.0.M3";
 	}
 
 	/**
 	 * @noreference
 	 */
 	public String getMweBuildNumber() {
-		return "S202607030623";
+		return "S202608160438";
 	}
 
 	public String getAntlrGeneratorVersion() {
